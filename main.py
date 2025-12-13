965354b4ce136bc3397d4f188ce70ecb2835d972
DavidShamoevi <davidbaloj@gmail.com>
2025-12-13 16:24:12 +0100

    Update main.py
    from export ergänzt mit export-button

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from components.editor.editor import Editor
 from components.timer.timer import Timer
 from components.todo.todo_list import TodoList
+from components.export.html_exporter import export_editor_to_html
+from pathlib import Path
 
 def main():
     root = tk.Tk()
@@ -17,6 +19,16 @@ def main():
     #test
     highlight_button = tk.Button(root, text="Highlight", command=lambda: editor.highlight_selection("markierung"))
     highlight_button.pack()
+
+    export_button = tk.Button(
+        root,
+        text="Export HTML",
+        command=lambda: export_editor_to_html(
+            editor,
+            filename=str(Path(__file__).resolve().parent / "export.html")
+        )
+    )
+    export_button.pack()
 
     todo_list = TodoList(root)
     todo_list.pack(fill=tk.BOTH, expand=True)
